21c5d2157379ba3b42eee0d3c2f4133433add5f7
bokwoon <bokwoon.c@gmail.com>
2019-02-09 03:48:57 +0800

    got server comms working + AES encryption

--- test/client.py
+++ test/client.py
@@ -1,7 +1,46 @@
 import socket
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+import time
+
+
+def decryptText(b64ascii, secret_key):
+    # decrypt base64 ascii into byte array
+    bytearr = base64.b64decode(b64ascii)
+    # Extract initialization vector iv (first 16 bytes) from byte array
+    iv = bytearr[0:16]
+    # Initialize cipher with secret_key and iv
+    cipher = AES.new(secret_key.encode('utf-8'), AES.MODE_CBC, iv)
+    # Decrypt string
+    decryptedString = cipher.decrypt(bytearr[16:]).decode('utf-8')
+    # Get everything after the first '#' character
+    decryptedString_subslice = decryptedString[decryptedString.find('#') + 1:]
+    action = decryptedString_subslice.split('|')[0]
+    voltage = decryptedString_subslice.split('|')[1]
+    current = decryptedString_subslice.split('|')[2]
+    power = decryptedString_subslice.split('|')[3]
+    cumpower = decryptedString_subslice.split('|')[4]
+    return {'action': action, 'voltage': voltage, 'current': current, 'power': power, 'cumpower': cumpower}
+
+
+def encryptText(dictt, secret_key):
+    def pad(s):
+        "pads string to a multiple of 16 chars"
+        return s.rjust(len(s) + 16 - len(s) % 16, "0")
+    strarray = list(map(str, dictt.values()))
+    payload = pad("#" + "|".join(strarray) + "|")
+    # Generate random 16 byte initialization vector
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(secret_key.encode('utf-8'), AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(payload))
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 8888))
-client.send("I am CLIENT\n".encode('utf-8'))
-from_server = client.recv(4096)
+strr = encryptText({'action': "chicken", 'voltage': 0, 'current': 0, 'power': 0, 'cumpower': 0}, "0123456789ABCDEF")
+time.sleep(60)
+client.send(strr)
+# from_server = client.recv(4096)
+# print(from_server.decode('utf-8'))
 client.close()
-print(from_server.decode('utf-8'))
--- test/server.py
+++ test/server.py
@@ -1,16 +1,42 @@
 import socket
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+
+
+def betterDecryptText(b64ascii, secret_key):
+    # decrypt base64 ascii into byte array
+    bytearr = base64.b64decode(b64ascii)
+    # Extract initialization vector iv (first 16 bytes) from byte array
+    iv = bytearr[0:16]
+    # Initialize cipher with secret_key and iv
+    cipher = AES.new(secret_key.encode('utf-8'), AES.MODE_CBC, iv)
+    # Decrypt string
+    decryptedString = cipher.decrypt(bytearr[16:]).decode('utf-8')
+    # Get everything after the first '#' character
+    decryptedString_subslice = decryptedString[decryptedString.find('#') + 1:]
+    action = decryptedString_subslice.split('|')[0]
+    voltage = decryptedString_subslice.split('|')[1]
+    current = decryptedString_subslice.split('|')[2]
+    power = decryptedString_subslice.split('|')[3]
+    cumpower = decryptedString_subslice.split('|')[4]
+    return {'action': action, 'voltage': voltage, 'current': current, 'power': power, 'cumpower': cumpower}
+
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('127.0.0.1', 8888))
 serv.listen(5)
+
 while True:
     conn, addr = serv.accept()
     from_client = ''
     while True:
         data = conn.recv(4096)
-        if not data: break
-        data = data.decode('utf-8')
-        from_client += data
-        print(from_client)
-        conn.send("I am SERVER\n".encode('utf-8'))
+        try:
+            decodedmsg = betterDecryptText(data.decode('utf-8'), "0123456789ABCDEF")
+            print(decodedmsg)
+        except Exception:
+            break
+        conn.send("I RECEIVED YOUR MESSAGE\n".encode('utf-8'))
     conn.close()
     print('client disconnected')
