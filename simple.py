d56c56764dc49d3645fbb8bbc7fac3b6ed8a4c05
bokwoon <bokwoon.c@gmail.com>
2019-03-07 12:34:22 +0800

    sorted out SYN_ACK and SYN_AXK_int

--- simple.py
+++ simple.py
@@ -1,6 +1,7 @@
 import serial
 import struct
 import time
+import csv
 
 # ser = serial.Serial("/dev/serial1", 115200, timeout=1, bytesize=8, parity='N', stopbits=1)
 
@@ -23,29 +24,22 @@ ser = serial.Serial(
 ACK = b'\x00'
 SYN = b'\x01'
 SYN_ACK = b'\x02'
-SYN_ACK_int = int.from_bytes(SYN_ACK, byteorder='big', signed=True)
 DATA_R = b'\x03'
 DATA_P = b'\x04'
 
 handshake_done = False
 
-# Handshake Beteen Rasberry Pi and Arduino
-
 
 def handshake(handshake_flag):
+    "Handshake Between Rasberry Pi and Arduino"
+    def sanitize_byte(bt):
+        "converts b'1' -> b'\x01'"
+        return bytes.fromhex(bt.decode('utf-8').rjust(2,'0'))
     while handshake_flag:
         time.sleep(1)
         ser.write(SYN)  # Request for handshake to start
-        string = ser.read()
-        reply = int.from_bytes(string, byteorder='big', signed=True) - 48
-        print(type(reply))
-        print(str(SYN_ACK_int))
-        print(type(string))
-        print(str(string))
-        print(type(SYN_ACK))
-        print(str(SYN_ACK))
-        print(string == SYN_ACK)
-        if (reply == SYN_ACK_int):
+        val = ser.read()
+        if (sanitize_byte(val) == SYN_ACK):
             handshake_flag = False
             ser.write(ACK)
     print('Handshake completed')
@@ -63,6 +57,7 @@ def getIMUPacket():
         if (endByte == 'E'):
             unpacked_data = struct.unpack('<bHHHHHHH', dataBytes)
             print(unpacked_data)
+    return unpacked_data
 
 def getPowerPacket():
     ser.write(DATA_P)  # Request for arduino to send data over
@@ -73,7 +68,7 @@ def getPowerPacket():
         if (endByte == 'E'):
             unpacked_data = struct.unpack('<HHHH', dataBytes)
             print(unpacked_data)
-            
+
 def getData(label, duration):
         arr_2d = []
         curr_time = time.time()
